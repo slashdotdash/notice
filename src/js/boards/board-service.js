00c57f4ab0e0cf686262ac6c1fb5b02920d69b34
Ben Smith <ben@slashdotdash.net>
2014-09-18 14:09:05 +0100

    Initial form to add a new card

--- src/js/boards/board-service.js
+++ src/js/boards/board-service.js
@@ -10,12 +10,17 @@ angular.module('app.boards')
             id: 1,
             title: 'To Do',
             cards: [
-              { id: 1, title: 'Add card', description: '' },
               { id: 2, title: 'Drag & drop card', description: '' },
               { id: 3, title: 'Create list', description: '' }
             ]
           },
-          { id: 2, title: 'Doing', cards: [] },
+          { 
+            id: 2,
+            title: 'Doing',
+            cards: [
+              { id: 1, title: 'Add card', description: '' }
+            ]
+          },
           { id: 3, title: 'Done', cards: [] }
         ]
       }
